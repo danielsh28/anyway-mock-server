202cbcf7a6488e49f70f3cfb5b3034d6e7ffc38d
danielsh28 <danielshely1@gmail.com>
2020-03-21 17:45:12 +0200

    first commit, created cache for any newsFlash newsflash

---
+++ routes/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const  fetch = require('node-fetch');
+const NodeCache = require('node-cache');
+const CACHE_KEY = 'lastNews';
+const ttl = 1000*5;
+const newsCache = new NodeCache();
+
+const  getNewsFromApi = () => {
+    return fetch('https://anyway.co.il/api/news-flash-filters?news_flash_count=10')
+        .then((realServerRes)=>realServerRes.json())
+};
+
+async function initCache(){
+    console.log(newsCache.getTtl(CACHE_KEY));
+    await getNewsFromApi().then((jsonObject) => {
+        newsCache.set(CACHE_KEY, jsonObject);
+    })
+};
+initCache();
+setInterval(initCache,ttl);
+
+async function getNewsFromCache(fetchApiFunc){
+    let val;
+    while(!val){
+        val = newsCache.get(CACHE_KEY);
+    }
+    return val;
+}
+
+
+
+router.get('/' ,async function (req,res){
+    const   newsdata = await getNewsFromCache();
+    res.send(newsdata);
+});
+
+
+module.exports= router;
